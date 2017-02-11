c8da0644899c0e4a73a77f800c6efa4e625419a8
Avi Cavale <avi@shippable.com>
2017-02-11 11:56:01 +0530

    standardizing docker build file

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM tomcat:8.0.20-jre8
 
-RUN rm -rf /usr/local/tomcat/webapps/*
-
-COPY sample.war /usr/local/tomcat/webapps/sample.war
+RUN rm -rf /usr/local/tomcat/webapps/HelloWorld.war
+COPY HelloWorld.war /usr/local/tomcat/webapps/HelloWorld.war
--- build.sh
+++ build.sh
@@ -1,48 +1,86 @@
 #!/bin/bash -e
 
-export BRANCH=docker
-export IMAGE_NAME=avinci/sample
-export IMAGE_TAG=$BRANCH.$BUILD_NUMBER
-export RES_DOCKER_CREDS=docker-creds
-export RES_REPO=demo-doc-repo
-export RES_IMAGE=demo-doc-img
+export CURR_JOB_PATH=$(eval echo "$"$JOB_NAME"_PATH") #where the curr build is running
 
-dockerLogin() {
-  echo "Extracting docker creds"
-  . ./IN/$RES_DOCKER_CREDS/integration.env
-  echo "logging into Docker with username" $username
-  docker login -u $username -p $password -e $email
+export REPO_RES="demo_doc_repo"
+export DOC_CREDS_RES="docker_creds"
+export DOC_IMG_RES="demo_doc_img"
+
+export REPO_RES_UP=$(echo $REPO_RES | awk '{print toupper($0)}')
+export REPO_RES_STATE=$(eval echo "$"$REPO_RES_UP"_STATE") #loc of git repo clone
+
+export DOC_CREDS_RES_UP=$(echo $AWS_CREDS_RES | awk '{print toupper($0)}')
+export DOC_CREDS_RES_INT_STR=$DOC_CREDS_RES_UP"_INTEGRATION"
+
+export DOC_IMG_RES_UP=$(echo $DOC_IMG_RES | awk '{print toupper($0)}')
+export DOC_IMG_SOURCENAME=$(eval echo "$"$DOC_IMG_RES_UP"_SOURCENAME") #lwhere image name is stored
+export IMAGE_TAG=$BUILD_NUMBER.$BUILD_JOB_NUMBER
+
+test_env_info() {
+
+  export DH_USERNAME=$(eval echo "$"$DOC_CREDS_RES_INT_STR"_USERNAME")
+  export DH_PASSWORD=$(eval echo "$"$DOC_CREDS_RES_INT_STR"_PASSWORD")
+  export DH_EMAIL=$(eval echo "$"$DOC_CREDS_RES_INT_STR"_EMAIL")
+
+  echo "Testing build ENV"
+
+  echo "########### CURR_JOB_PATH: $CURR_JOB_PATH"
+
+  echo "########### REPO_RES: $REPO_RES"
+  echo "########### REPO_RES_UP: $REPO_RES_UP"
+  echo "########### REPO_RES_STATE: $REPO_RES_STATE"
+
+  echo "########### DOC_CREDS_RES: $DOC_CREDS_RES"
+  echo "########### DOC_CREDS_RES_UP: $DOC_CREDS_RES_UP"
+  echo "########### DH_USERNAME: $DH_USERNAME"
+  echo "########### DH_PASSWORD: ${#DH_PASSWORD}"
+  echo "########### DH_EMAIL: $DH_EMAIL"
+
+  echo "########### DOC_IMG_RES: $DOC_IMG_RES"
+  echo "########### DOC_IMG_RES_UP: $DOC_IMG_RES_UP"
+  echo "########### DOC_IMG_SOURCENAME: $DOC_IMG_SOURCENAME"
+  echo "########### IMAGE_TAG: $IMAGE_TAG"
+
+  sudo wget http://www-us.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
+  sudo tar xzf apache-maven-3.3.9-bin.tar.gz -C /usr/local && rm -f apache-maven-3.3.9-bin.tar.gz
+  ln -fs /usr/local/apache-maven-$3.3.9/bin/mvn /usr/bin
+  echo "export PATH=$PATH:/usr/local/apache-maven-3.3.9/bin" >> $HOME/.bashrc
+
+  echo "logging into Docker with username" $DH_USERNAME
+  docker login -u $DH_USERNAME -p $DH_PASSWORD -e $DH_EMAIL
   echo "Completed Docker login"
+
+  echo "Build ENV is good"
 }
 
-dockerBuild() {
-  echo "Starting Docker build for" $IMAGE_NAME:$IMAGE_TAG
-  cd ./IN/$RES_REPO/gitRepo
-  wget http://138.197.80.53:8080/docs/appdev/sample/sample.war
-  pwd
-  ls -al
-  sudo docker build -t=$IMAGE_NAME:$IMAGE_TAG .
-  echo "Completed Docker build for" $IMAGE_NAME:$IMAGE_TAG
+build_push() {
+  popd $REPO_RES_STATE
+
+  echo "Building WAR file"
+  mkdir -p shippable/testresults && mkdir -p shippable/codecoverage
+  /usr/local/apache-maven-3.3.9/bin/mvn -q -B clean cobertura:cobertura install
+  ls -al ./target
+  echo "Completed WAR file build"
+
+  echo "Starting Docker build & push for" $DOC_IMG_SOURCENAME:$IMAGE_TAG
+  sudo docker build -t=$DOC_IMG_SOURCENAME:$IMAGE_TAG .
+  sudo docker push $DOC_IMG_SOURCENAME:$IMAGE_TAG
+  echo "Completed Docker build & push for" $DOC_IMG_SOURCENAME:$IMAGE_TAG
+
+  popd
 }
 
-dockerPush() {
-  echo "Starting Docker push for" $IMAGE_NAME:$IMAGE_TAG
-  sudo docker push $IMAGE_NAME:$IMAGE_TAG
-  echo "Completed Docker push for" $IMAGE_NAME:$IMAGE_TAG
-}
-
-createOutState() {
-  echo "Creating a state file for" $RES_IMAGE
-  echo versionName=$IMAGE_TAG > /build/state/$RES_IMAGE.env
-  cat /build/state/$RES_IMAGE.env
-  echo "Completed creating a state file for" $RES_IMAGE
+create_state() {
+  echo "Creating a state file for" $DOC_IMG_RES
+  echo versionName=$IMAGE_TAG > /build/state/$DOC_IMG_RES.env
+  cat /build/state/$DOC_IMG_RES.env
+  echo "Completed creating a state file for" $DOC_IMG_RES
 }
 
 main() {
-  dockerLogin
-  dockerBuild
-  dockerPush
-  createOutState
+  test_env_info
+#  build_push
+#  create_state
 }
 
 main
