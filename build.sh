dd8e8a51e97c2ecff948be9e5b6180e23b68215f
Avi Cavale <avi@shippable.com>
2017-02-11 13:52:30 +0530

    moving to just downloading WAR

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 
-
 export JOB_NAME_UP=$(echo $JOB_NAME | awk '{print toupper($0)}')
 export CURR_JOB_PATH=$(eval echo "$"$JOB_NAME_UP"_PATH") #where the curr build is running
 
@@ -19,7 +18,6 @@ export DOC_IMG_SOURCENAME=$(eval echo "$"$DOC_IMG_RES_UP"_SOURCENAME") #lwhere i
 export IMAGE_TAG=$BUILD_NUMBER.$BUILD_JOB_NUMBER
 
 test_env_info() {
-
   export DH_USERNAME=$(eval echo "$"$DOC_CREDS_RES_INT_STR"_USERNAME")
   export DH_PASSWORD=$(eval echo "$"$DOC_CREDS_RES_INT_STR"_PASSWORD")
   export DH_EMAIL=$(eval echo "$"$DOC_CREDS_RES_INT_STR"_EMAIL")
@@ -43,33 +41,9 @@ test_env_info() {
   echo "########### DOC_IMG_SOURCENAME: $DOC_IMG_SOURCENAME"
   echo "########### IMAGE_TAG: $IMAGE_TAG"
 
-  sudo dpkg --configure -a
-  sudo add-apt-repository ppa:webupd8team/y-ppa-manager
-  sudo apt-get update
-  sudo apt-get install y-ppa-manager
-
-  add-apt-repository -y ppa:openjdk-r/ppa
-  apt-get update
-  apt-get install -y openjdk-8-jdk
-  update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
-  update-alternatives --set javac /usr/lib/jvm/java-8-openjdk-amd64/bin/javac
-  apt-get install --reinstall ca-certificates
-  add-apt-repository ppa:maarten-fonville/ppa
-  apt-get update
-  apt-get install -y icedtea-8-plugin
-  update-alternatives --set javaws /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/javaws
-
-  echo "########### JAVA_HOME: $JAVA_HOME"
-  which java
-
-  sudo wget http://www-us.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
-  sudo tar xzf apache-maven-3.3.9-bin.tar.gz -C /usr/local && rm -f apache-maven-3.3.9-bin.tar.gz
-  ln -fs /usr/local/apache-maven-$3.3.9/bin/mvn /usr/bin
-  echo "export PATH=$PATH:/usr/local/apache-maven-3.3.9/bin" >> $HOME/.bashrc
-
-  echo "logging into Docker with username" $DH_USERNAME
-  docker login -u $DH_USERNAME -p $DH_PASSWORD -e $DH_EMAIL
-  echo "Completed Docker login"
+#  echo "logging into Docker with username" $DH_USERNAME
+#  docker login -u $DH_USERNAME -p $DH_PASSWORD -e $DH_EMAIL
+#  echo "Completed Docker login"
 
   echo "Build ENV is good"
 }
@@ -77,11 +51,9 @@ test_env_info() {
 build_push() {
   pushd $REPO_RES_STATE
 
-  echo "Building WAR file"
-  mkdir -p shippable/testresults && mkdir -p shippable/codecoverage
-  /usr/local/apache-maven-3.3.9/bin/mvn -q -B clean cobertura:cobertura install
-  ls -al ./target
-  echo "Completed WAR file build"
+  jfrog rt dl builds/HelloWorld.war
+
+  ls -al
 
   echo "Starting Docker build & push for" $DOC_IMG_SOURCENAME:$IMAGE_TAG
   sudo docker build -t=$DOC_IMG_SOURCENAME:$IMAGE_TAG .
